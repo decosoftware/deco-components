e534472ecfc82ee8495a15bf6f8db2ea1186ff44
Devin Abbott <devinabbott@gmail.com>
2016-05-25 16:09:04 -0700

    [update] Lightbox: add width, so it appears even if align-items center.

--- official/Lightbox.jsx
+++ official/Lightbox.jsx
@@ -1,6 +1,9 @@
 <Lightbox navigator={this.props.navigator}>
   <Image 
-    style={{height: 200}}
+    style={{
+      height: 200, 
+      width: 300,
+    }}
     source={{uri:'https://unsplash.it/600/400/?random'}}
   />
 </Lightbox>
